41e2fd0c1f9d039558b17909849c5ae63cdf1a42
EdPanich <eduard.panich12@gmail.com>
2025-05-15 02:28:56 -0700

    AA

--- decante/decante_app/apps.py
+++ decante/decante_app/apps.py
@@ -5,15 +5,3 @@ from django.db.utils import OperationalError
 class DecanteAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'decante_app'
-
-    def ready(self):
-        from django.contrib.auth.models import User
-        try:
-            if not User.objects.filter(username='admin').exists():
-                User.objects.create_superuser(
-                    username='admin',
-                    email='admin@decante.com',
-                    password='admin123'
-                )
-        except OperationalError:
-            pass
