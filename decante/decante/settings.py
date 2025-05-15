72a718ddd9fb6123c3ab9dfe489d58023483011a
EdPanich <eduard.panich12@gmail.com>
2025-05-15 02:30:49 -0700

    22

--- decante/decante/settings.py
+++ decante/decante/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'decante_app.apps.DecanteAppConfig',
+    'decante_app',
 ]
 
 MIDDLEWARE = [
--- decante/decante/urls.py
+++ decante/decante/urls.py
@@ -23,6 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', lambda request: redirect('lista_deudores')),
     path('', include('decante_app.urls')),
-    path('login/', auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
